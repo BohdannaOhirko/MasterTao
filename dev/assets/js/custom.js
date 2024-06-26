c75caabbb37901f6b0882dfcef7b34662f0e3476
Bohdanna <bohdannaohirko@.gmail.comgit config --global user.name Bohdannagit config --global user.email bohdannaohirko@.gmail.com>
2024-06-26 15:13:52 +0300

    header desktop done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,5 +1,12 @@
-$( document ).ready(function() {
-   $('.single-item').slick(); 
-   console.log( "ready!" );
-});
+// $( document ).ready(function() {
+//    $('.single-item').slick(); 
+//    console.log( "ready!" );
+// });
 
+const burger = document.querySelector('#burger');
+const mobileContainer = document.querySelector('.mobile-container');
+function showBurgerMenu(){
+  mobileContainer.classList.toggle('show');
+}
+showBurgerMenu();
+burger.addEventListener('click', showBurgerMenu);
