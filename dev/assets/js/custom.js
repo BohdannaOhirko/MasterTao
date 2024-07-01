4a5979a090d46ccb1c00939a703d21f7c15c737f
Bohdanna <bohdannaohirko@.gmail.comgit config --global user.name Bohdannagit config --global user.email bohdannaohirko@.gmail.com>
2024-07-01 13:07:13 +0300

    first screen desktop done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,13 +1,32 @@
 // $( document ).ready(function() {
-//    $('.single-item').slick(); 
+//    $('.single-item').slick();
 //    console.log( "ready!" );
 // });
 
-const burger = document.querySelector('#burger');
-const mobileContainer = document.querySelector('.mobile-container');
-function showBurgerMenu(){
-  burger.classList.toggle('open');
-  mobileContainer.classList.toggle('show');
+const burger = document.querySelector("#burger");
+const mobileContainer = document.querySelector(".mobile-container");
+burger.classList.toggle("open");
+mobileContainer.classList.toggle("show");
+function showBurgerMenu() {
+	burger.classList.toggle("open");
+	mobileContainer.classList.toggle("show");
 }
 showBurgerMenu();
-burger.addEventListener('click', showBurgerMenu);
+burger.addEventListener("click", showBurgerMenu);
+
+const swiper = new Swiper(".slider-info", {
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+
+	// autoplay: {
+	// 	delay: 3000,
+	// },
+});
