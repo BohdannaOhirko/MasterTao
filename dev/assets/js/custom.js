bfe9fad787228cf8aec2a1eb886a1833b0321bd4
Bohdanna <bohdannaohirko@.gmail.comgit config --global user.name Bohdannagit config --global user.email bohdannaohirko@.gmail.com>
2024-07-22 15:28:43 +0300

    sider in js has not done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -44,21 +44,76 @@ const fileLable = document.querySelectorAll(".calculation-label");
 const fileInput = document.querySelectorAll(".calculation-file");
 
 fileInput.forEach((item) => {
-	console.log(item);
+	// console.log(item);
 
 	item.addEventListener("change", function () {
 		// let fileName = item.previousElementSibling; і так теж можна
 		// console.log(fileName);
-		let fileName = item.closest('.input-box').querySelector('.file-name')
-        fileName.innerHTML = item.files[0].name;
+		let fileName = item.closest(".input-box").querySelector(".file-name");
+		fileName.innerHTML = item.files[0].name;
 	});
 });
 // console.log(fileInput);
 fileLable.forEach((item) =>
 	item.closest(".form-row").previousElementSibling.classList.add("special-gap")
 );
+
 // fileInput.addEventListener("change", () => {
 // 	fileName.innerHTML = fileInput.files[0].name;
 // });
 
 // fileInput.closest(".form-row").classList.add("special-gap");
+/*swiperAdvantages*/
+let swiperAdvantages ;
+
+function initAdvantages() {
+    swiperAdvantages = new Swiper('.slider-advantages', {
+    spaceBetween: 12,
+    loop: true,
+  
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        enabled: true,
+    },
+    breakpoints: {
+        1025: {
+            spaceBetween: 40,
+            navigation: {
+                enabled: true,
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            pagination: {
+                enabled: false,
+            },
+  
+        }
+    }
+  });
+}
+
+function destroyAdvantages() {
+  swiperAdvantages.destroy( true, true )   
+}
+
+
+function mediaQuery(matchMedia1024) {
+    if (matchMedia1024.matches) { // If media query matches
+      initAdvantages()
+    } else {
+      destroyAdvantages()
+    }
+}
+
+// Create a MediaQueryList object
+const matchMedia1024 = window.matchMedia("(max-width: 1024px)")
+
+// Call listener function at run time
+mediaQuery(matchMedia1024);
+
+// Attach listener function on state changes
+matchMedia1024.addEventListener("change", function() {
+    mediaQuery(matchMedia1024);
+});
+/**/
